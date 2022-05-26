a58748487deafd9f5fec2375b267d9fed6be84d0
rtokkie <natto.and.eggs@gmail.com>
2022-05-26 21:47:03 +0900

    f

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import "reflect-metadata";
-
-import { container } from "tsyringe";
-
-import { Bar } from "./bar";
-
-const bar = container.resolve(Bar);
-
-console.log(bar);
+import "./tsyringe-impl";
